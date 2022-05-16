1f3393bf48f7ee3c344a84ae61b43e7847a1c236
Islam <islamkazproject@gmail.com>
2022-05-16 12:45:09 +0300

    finally version

--- begining_zero.py
+++ begining_zero.py
@@ -4,26 +4,21 @@
 #Выходные данные: Целое число.
 def beginning_zeros(number: str) -> int:
     # your code here
-
-    # Если в строке одни нули
-    if number == '0'*(len(number)):
-        return len(number)
-    # к обычному числу
-    num_reverse = "".join(reversed(number))
-    num_int = int(num_reverse)
     count = 0
-    while num_int > 0:
-        if num_int % 10 == 0:
+    for letter in number:
+        if letter == '0':
             count += 1
-            num_int = num_int / 10
         else:
             break
     return count
+        
+    
 
 
 if __name__ == '__main__':
     print("Example:")
     print(beginning_zeros('100'))
+    print(beginning_zeros('123456'))
 
     # These "asserts" are used for self-checking and not for an auto-testing
     assert beginning_zeros('100') == 0
